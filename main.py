4b9f76e8fc5a6ba90bc548b9106cc483d69e27a4
Yahya4noshaily <nushily4@gmail.com>
2025-08-02 04:12:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# Main bot execution
 from browser import run_browser
 
-run_browser()
+# بيانات تسجيل الدخول
+email = "Nushily4@gmail.com"
+password = "Ya12345678"
+market = "smarty"        # تقدر تغيّره إلى أي سوق آخر
+duration = 30            # المدة بالثواني للبقاء على الشارت
+
+# تشغيل المتصفح والتحليل
+run_browser(email, password, market, duration)
